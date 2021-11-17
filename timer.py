6e52ba8f2d2a5d84b61f32c3c6235460a0e98249
James D. Mitchell <jdm3@st-andrews.ac.uk>
2021-11-17 12:32:34 +0000

    Update in 2021

--- timer.py
+++ timer.py
@@ -1,8 +1,10 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 """
+A simple script for timing exam marking per script.
 """
 
 import statistics
+import sys
 import time
 
 
@@ -12,19 +14,33 @@ def time_string(val):
 
 last_time = time.time()
 times = []
-
 while True:
-    input()
+    val = input(
+        "Press return when you've marked the next script or escape to stop!"
+    )
+    if val == "\u001B":
+        if len(times) != 0:
+            print(
+                "mean %s, min %s, count %d, total %s"
+                % (
+                    time_string(statistics.mean(times)),
+                    time_string(min(times)),
+                    len(times),
+                    time_string(sum(times)),
+                )
+            )
+        sys.exit(0)
     now = time.time()
     elapsed = now - last_time
     last_time = now
     times.append(elapsed)
     print(
-        "last script marked in %s, mean %s, count %d, total %s"
+        "last script marked in %s, mean %s, min %s, count %d, total %s"
         % (
             time_string(elapsed),
             time_string(statistics.mean(times)),
+            time_string(min(times)),
             len(times),
-            time_string(sum(times))
+            time_string(sum(times)),
         )
     )
