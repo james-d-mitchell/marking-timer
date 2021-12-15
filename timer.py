729a1d6e69549cab744847e476fdfc1996c36eb7
James D. Mitchell <jdm3@st-andrews.ac.uk>
2021-12-15 11:15:56 +0000

    Latest iteration

--- timer.py
+++ timer.py
@@ -10,25 +10,38 @@ import time
 def time_string(val):
     return "%dm%ds" % (int(val) / 60, int(val % 60))
 
+
 def pause_until_resumed():
     input("PAUSED. Press return to start the next script.")
+
 
 def quartiles(vals):
     vals = sorted(vals)
     n = len(vals)
-    return [vals[0], vals[round(n/4)], vals[n//2], vals[(3*n)//4], vals[-1]]
+    return [
+        vals[0],
+        vals[round(n / 4)],
+        vals[n // 2],
+        vals[(3 * n) // 4],
+        vals[-1],
+    ]
 
 
 times = []
-paused_time = 0
 try:
+    extra_time = 0
     while True:
         last_time = time.time()
         from_user = input(
-            "Press return when you've marked the next script or ctrl-c to stop! Enter p to pause"
+            "Press return when you've marked the next script or ctrl-c to stop!  Enter p to pause: "
         )
+        if from_user in ["p", "P"]:
+            extra_time = time.time() - last_time
+            pause_until_resumed()
+            continue
         now = time.time()
-        elapsed = now - last_time
+        elapsed = now - last_time + extra_time
+        extra_time = 0
         times.append(elapsed)
         print(
             "\033[1m ===> number %d marked in %s, mean %s, total %s\033[0m"
@@ -39,11 +52,12 @@ try:
                 time_string(sum(times)),
             )
         )
-        print("\033[1m ===> quartiles |%s-[%s-%s-%s]-%s|\033[0m" % tuple(map(time_string, quartiles(times))))
+        print(
+            "\033[1m ===> quartiles |%s-[%s-%s-%s]-%s|\033[0m"
+            % tuple(map(time_string, quartiles(times)))
+        )
 
-        if from_user in ['p', 'P']:
-            pause_until_resumed()
-            
+
 except KeyboardInterrupt:
     if len(times) != 0:
         print(
